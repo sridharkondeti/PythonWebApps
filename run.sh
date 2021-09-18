720016a69aa1b39b58d71fe4c02dc656b33716a3
sridharkondeti <sridharreddy2kx@gmail.com>
2021-09-18 16:33:56 -0600

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,22 @@
+# Install Docker
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo apt-get update
+
+sudo apt-get install     apt-transport-https     ca-certificates     curl     gnupg     lsb-release
+
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+       $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+
+# Build Docker image
+sudo docker build -t python-hw .
+
+# Run Docker image
+sudo docker run -p 5000:5000 python-hw
